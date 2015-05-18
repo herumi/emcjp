d42670391c7c92aa4e13ce9c8b2db2f35e8c66e3
MITSUNARI Shigeo <herumi@nifty.com>
2015-05-18 15:53:07 +0900

    add result

--- src/item21-3.cpp
+++ src/item21-3.cpp
@@ -1,4 +1,28 @@
-// cl item21-3.cpp /MT
+/*
+cl item21-3.cpp /MT
+
+           /DUSE_MAKE_SHARED
+0 40       0 40
+1 5781     1 5781
+2 552      2 552
+3 29       3 29
+4 4096     4 4096
+5 16   ;   5 32   ;
+6 24   ;   6 32   ;
+7 16   ;   7 32   ;
+8 24   ;
+9 16   ;
+10 24  ;
+
+gcc
+
+0 16   ;  0 40
+1 24   ;  1 40
+2 16   ;  2 40
+3 24
+4 16
+5 24
+*/
 #include <memory>
 #include <stdio.h>
 #include <stdlib.h>
