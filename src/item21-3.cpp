d6c4f347195818adf186d7dc4e6a401cb6d88637
MITSUNARI Shigeo <herumi@nifty.com>
2015-05-18 15:49:39 +0900

    # of malloc by make_shared

---
+++ src/item21-3.cpp
@@ -0,0 +1,47 @@
+#include <memory>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int pos = 0;
+static struct Mmem {
+	char buf[8192];
+	size_t size;
+} mem[4096];
+
+extern "C" void *malloc(size_t n) throw()
+{
+	if (n >= 8192) {
+		puts("ERR");
+		exit(1);
+	}
+	mem[pos].size = n;
+	return mem[pos++].buf;
+}
+extern "C" void free(void *p) throw()
+{
+	if (p == 0) return;
+	printf("free %p\n", p);
+}
+
+struct A {
+	int a[4];
+};
+
+void f()
+{
+#ifdef USE_MAKE_SHARED
+	auto p = std::make_shared<A>();
+#else
+	std::shared_ptr<A> p(new A());
+#endif
+}
+
+int main()
+{
+	for (int i = 0; i < 3; i++) {
+		f();
+	}
+	for (int i = 0; i < pos; i++) {
+		printf("%d %zd\n", i, mem[i].size);
+	}
+}
